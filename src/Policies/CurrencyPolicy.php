4fa5efcd99918a3a1f7d3615fbd9e1faca9a7e44
adrianzofcin <75702986+adrianzofcin@users.noreply.github.com>
2024-09-04 10:37:16 +0200

    Update CurrencyPolicy.php

--- src/Policies/CurrencyPolicy.php
+++ src/Policies/CurrencyPolicy.php
@@ -6,67 +6,68 @@ namespace Wame\LaravelNovaCurrency\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class CurrencyPolicy
 {
     use HandlesAuthorization;
 
 
-    public function viewAny(User $user)
+    public function viewAny(User|Authenticatable $user)
     {
         return true;
     }
 
 
-    public function view(User $user, $model)
+    public function view(User|Authenticatable $user, $model)
     {
         return true;
     }
 
 
-    public function create(User $user)
+    public function create(User|Authenticatable $user)
     {
         return false;
     }
 
 
-    public function update(User $user, $model)
+    public function update(User|Authenticatable $user, $model)
     {
         return true;
     }
 
 
-    public function replicate(User $user, $model)
+    public function replicate(User|Authenticatable $user, $model)
     {
         return false;
     }
 
 
-    public function delete(User $user, $model)
+    public function delete(User|Authenticatable $user, $model)
     {
         return false;
     }
 
 
-    public function forceDelete(User $user, $model)
+    public function forceDelete(User|Authenticatable $user, $model)
     {
         return false;
     }
 
 
-    public function restore(User $user, $model)
+    public function restore(User|Authenticatable $user, $model)
     {
         return false;
     }
 
 
-    public function runAction(User $user)
+    public function runAction(User|Authenticatable $user)
     {
         return false;
     }
 
 
-    public function runDestructiveAction(User $user)
+    public function runDestructiveAction(User|Authenticatable $user)
     {
         return true;
     }
